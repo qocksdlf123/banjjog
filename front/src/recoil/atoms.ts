a382d61792e4124f480d1c200b88b607bf08ef1f
limzzum <chfhddl1234@naver.com>
2023-12-08 11:11:09 +0900

    :sparkles: feat : 전체 응답 완료 상태관리 리코일 추가

--- front/src/recoil/atoms.ts
+++ front/src/recoil/atoms.ts
@@ -1,5 +1,5 @@
 // atoms.ts
-import { atom } from "recoil";
+import { RecoilState, atom } from "recoil";
 
 export const countPage = atom({
   key: "countCurPage",
@@ -8,4 +8,8 @@ export const countPage = atom({
 export const countTotalPage = atom({
   key: "countTotalPage",
   default: 4,
+});
+export const endResponse: RecoilState<boolean> = atom({
+  key: "endResponse",
+  default: false,
 });
