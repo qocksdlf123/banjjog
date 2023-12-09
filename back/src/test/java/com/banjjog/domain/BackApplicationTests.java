db3e3526f07838c11315dd7b7ebe8e42ec6befb1
qocksdlf123 <135696863+qocksdlf123@users.noreply.github.com>
2023-12-09 15:36:12 +0900

    FIx) text 주석처리

--- back/src/test/java/com/banjjog/domain/BackApplicationTests.java
+++ back/src/test/java/com/banjjog/domain/BackApplicationTests.java
@@ -1,13 +1,13 @@
-package com.banjjog.domain;
+// package com.banjjog.domain;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BackApplicationTests {
+// @SpringBootTest
+// class BackApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
