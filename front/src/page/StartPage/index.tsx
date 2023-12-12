4eab82069d46a9a5371d30845becb7d5ae17d854
limzzum <chfhddl1234@naver.com>
2023-12-12 16:07:55 +0900

    :sparkles: feat : 사용자 이름 입력 공백 예외처리 추가

--- front/src/page/StartPage/index.tsx
+++ front/src/page/StartPage/index.tsx
@@ -2,6 +2,10 @@ import "./style.css";
 import Logo from "../../assets/MainPageAssets/Logo.png";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { useRecoilState } from "recoil";
+import { myNameState, banjjogNameState, userIdState } from "../../recoil/atoms";
+import { isExistUser, createtUser } from "../../api/UserAPI";
+import Swal from "sweetalert2";
 
 const StartPage = () => {
   return (
@@ -24,15 +28,16 @@ const Header = () => {
 };
 
 const Body = () => {
-  const [userName, setUserName] = useState<string>("");
-  const [banjjogName, setBanjjogName] = useState<string>("");
+  const [myName, setMyname] = useRecoilState<string>(myNameState);
+  const [banjjogName, setbanjjogname] =
+    useRecoilState<string>(banjjogNameState);
   const handleInputUserName = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUserName(event.target.value);
+    setMyname(event.target.value);
   };
   const handleInputBanjjogName = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
-    setBanjjogName(event.target.value);
+    setbanjjogname(event.target.value);
   };
   return (
     <div className="start-body">
@@ -41,7 +46,7 @@ const Body = () => {
         <input
           className="start-inputbox"
           placeholder="[입력] 사용자 성명 입력"
-          value={userName}
+          // value={userName}
           onChange={handleInputUserName}
         ></input>
       </div>
@@ -50,12 +55,14 @@ const Body = () => {
         <input
           className="start-inputbox"
           placeholder="[입력] 연인 성명 입력"
-          value={banjjogName}
+          // value={banjjogName}
           onChange={handleInputBanjjogName}
         ></input>
       </div>
       <div className="start-body-text">
-        나와 연인의 이름을 입력해야 서로의 결과를 비교해서 볼 수 있습니다
+        나와 연인의 이름을 입력해야
+        <br />
+        서로의 결과를 비교해서 볼 수 있습니다
       </div>
     </div>
   );
@@ -63,9 +70,37 @@ const Body = () => {
 
 const Footer = () => {
   const history = useNavigate();
+  const [myName, setMyname] = useRecoilState<string>(myNameState);
+  const [yourName, setbanjjogname] = useRecoilState<string>(banjjogNameState);
+  const [userId, setUserId] = useRecoilState<number>(userIdState);
+
   const startGame = () => {
+    if (myName == "" || yourName == "") {
+      Swal.fire({
+        title: "이름을 입력하세요!",
+        text: "나와 상대방의 이름은 공백으로 할 수 없습니다.",
+        icon: "error",
+        confirmButtonColor: "#4461F2",
+        confirmButtonText: "확인",
+        customClass: {
+          confirmButton: "swal-btn-login",
+          icon: "swal-icon-login",
+        },
+      });
+      return;
+    }
+    isExistUser({ myName, yourName }).then((response) => {
+      if (response.data == 0) {
+        createtUser({ myName, yourName }).then((response) => {
+          setUserId(response.data.userId);
+        });
+      } else {
+        setUserId(response.data);
+      }
+    });
     history("/gameList");
   };
+
   return (
     <div className="start-footer">
       <button onClick={startGame} className="main-start-btn">
