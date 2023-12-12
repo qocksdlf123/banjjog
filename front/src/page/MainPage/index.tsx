5e475f7ab6a4a23cfeb2108243fc4f6cd41286b5
limzzum <chfhddl1234@naver.com>
2023-12-12 16:05:54 +0900

    :sparkles: feat : 유저 수 count API 연결 추가

--- front/src/page/MainPage/index.tsx
+++ front/src/page/MainPage/index.tsx
@@ -1,6 +1,8 @@
 import "./style.css";
 import Logo from "../../assets/MainPageAssets/Logo.png";
 import { useNavigate } from "react-router-dom";
+import { getUserCount } from "../../api/UserAPI";
+import { useEffect, useState } from "react";
 
 const MainPage = () => {
   return (
@@ -26,15 +28,13 @@ const Body = () => {
   return (
     <div className="main-body">
       <div className="main-body-text">
-        반쪽은 연인과 함께 즐길 수 있는 ‘퀴즈형 게임’ 콘텐츠를 제공하고
+        반쪽은 연인과 함께 즐길 수 있는
+        <br />
+        ‘퀴즈형 게임’ 콘텐츠를 제공하고
+        <br />
         있습니다.
       </div>
-      <img
-        className="main-body-logo"
-        width="100px"
-        height="100px"
-        src={Logo}
-      ></img>
+      <img className="main-body-logo" src={Logo}></img>
     </div>
   );
 };
@@ -44,13 +44,24 @@ const Footer = () => {
   const startGame = () => {
     history("/start");
   };
+  const [userCount, setUserCount] = useState<number>();
+
+  useEffect(() => {
+    getUserCount()
+      .then((response) => {
+        setUserCount(response.data);
+      })
+      .catch((error) => {
+        console.log("getUserCount error : " + error.toString());
+      });
+  }, []);
   return (
     <div className="main-footer">
       <button onClick={startGame} className="main-start-btn">
         시작하기
       </button>
       <br></br>
-      <div>지금까지 00명이 참여했어요!</div>
+      <div>지금까지 {userCount}명이 참여했어요!</div>
     </div>
   );
 };
